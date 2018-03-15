6c3aa53f406fe638076ef3ad0fe0569304de2cd1
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-15 13:19:40 +0100

    php variable

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-    
-    $airports = [
-        'TXL' => 'Berlin - Tegel',
-        'SXF' => 'Berlin - Schönefeld'
-    ];
-    
+$airports = [];
+$airports[] = ['TXL' , 'Berlin - Tegel'];
+$airports[] = ['SXF' , 'Berlin - Schönefeld'];
+
+// Aufschlüsselung
+//$airports = [0];
+//$airports[0]['TXL'] = 'Berlin - Tegel';
+//$airports[] = ['TXL' => 'Berlin - Tegel'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,22 +26,34 @@
             <form autocomplete="off">
                 <fieldset>
                     <legend>Persönliche Daten</legend>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                        <label for="fn">Vorname</label>
-                        <input class="form-control" type="text" id="fn" name="firstName" value="">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                            <label for="fn">Vorname</label>
+                            <input class="form-control" type="text" id="fn" name="firstName" value="">
+                        </div>
+                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                            <label for="ln">Name</label>
+                            <input class="form-control" type="text" id="ln" name="lastName" value="" placeholder="Pflichtfeld" required >
+                        </div>
                     </div>
-                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                        <label for="ln">Name</label>
-                        <input class="form-control" type="text" id="ln" name="lastName" value="" placeholder="Pflichtfeld" required >
-                    </div>
-                </div>
                     <div class="row">
                         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 align-self-end">
                             <button formaction="update.php" name="btnAction" value="update" class="btn btn-primary" style="margin-top: 10px">Update</button>
                             <button formaction="insert.php" name="btnAction" value="insert" class="btn btn-primary" style="margin-top: 10px">Insert</button>
                         </div>
                     </div>
+                </fieldset>
+                <hr>
+                <fieldset>
+                    <legend>Auswahl des Flughafens</legend>
+                    <select class="form-control" name="airportStart">
+                        <option>Bitte Flughafen auswählen...</option>
+
+                        <?php for ($i = 0; $i < count($airports); $i++) : ?>
+                            <option value="<?php echo $airports[$i][0]; ?>"><?php echo $airports[$i][1]; ?></option>
+                        <?php endfor; ?>
+
+                    </select>
                 </fieldset>
             </form>
         </div>
