d02a0788e17011c5671c74f55ca5bb2f709bf2fc
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-15 11:23:39 +0100

    php variable

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
+<?php
+    
+    $airports = [
+        'TXL' => 'Berlin - Tegel',
+        'SXF' => 'Berlin - Schönefeld'
+    ];
+    
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP - 01 Basic</title>
+        <title>PHP - 04 Forms / Formulare</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
@@ -11,9 +19,29 @@
         <script src="assets/js/main.js" type="text/javascript"></script>
     </head>
     <body>
-        <h1>Bootstrap</h1>
         <div class="container">
-            
+            <h1>PHP - Formulare</h1>
+            <form autocomplete="off">
+                <fieldset>
+                    <legend>Persönliche Daten</legend>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <label for="fn">Vorname</label>
+                        <input class="form-control" type="text" id="fn" name="firstName" value="">
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <label for="ln">Name</label>
+                        <input class="form-control" type="text" id="ln" name="lastName" value="" placeholder="Pflichtfeld" required >
+                    </div>
+                </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 align-self-end">
+                            <button formaction="update.php" name="btnAction" value="update" class="btn btn-primary" style="margin-top: 10px">Update</button>
+                            <button formaction="insert.php" name="btnAction" value="insert" class="btn btn-primary" style="margin-top: 10px">Insert</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
         </div>
     </body>
 </html>
