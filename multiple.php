86fe72ed32d5724b8edd82203af29e181f1c61f8
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-16 10:29:44 +0100

    multiple.php angelegt

---
+++ multiple.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP - 04 Forms / Formulare</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <form>
+                
+            </form>
+        </div>
+        
+        <hr>
+        
+        <pre>
+            <?php
+                var_dump();
+            ?>
+        </pre>
+    </body>
+</html>
