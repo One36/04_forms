72b9eeb5a9b6af4e7a7d03e2a377dc7051c4f466
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-20 10:49:41 +0100

    20.03.2018

--- files.php
+++ files.php
@@ -1,4 +1,9 @@
 <?php
+$message = [
+    'text' => '',
+    'type' => 'info'
+];
+
 if (isset($_FILES['uploadFile'])) {
     $imageTypes = ['', '.gif', '.jpeg', '.png'];
     $src = $_FILES['uploadFile']['tmp_name'];
@@ -6,16 +11,28 @@ if (isset($_FILES['uploadFile'])) {
     $imgType = $imgInfo[2]; // 1(gif) 2(jpeg) 3(png)
 
     if ($imgType >= 1 && $imgType <= 3) {
-        
+
         $folder = './uploads/';
         $filename = uniqid('634287', true);
         $filetype = $imageTypes[$imgType];
-        
-        
+
+
         $dst = $folder . $filename . $filetype;
-        
-        
-        move_uploaded_file($src, $dst);
+
+        $message['text'] = 'Upload ';
+
+        if (move_uploaded_file($src, $dst)) {
+
+            $message['text'] .= 'erfolgreich';
+            $message['type'] = 'success';
+        } else {
+            $message['text'] .= 'fehlgeschlagen';
+            $message['type'] = 'danger';
+        }
+//        $message .= (move_uploaded_file($src, $dst)) ? 'erfolgreich' : 'fehlgeschlagen';
+    } else {
+        $message['text'] .= 'Falscher Dateityp';
+        $message['type'] = 'danger';
     }
 }
 ?>
@@ -44,14 +61,39 @@ if (isset($_FILES['uploadFile'])) {
                     <button class="btn btn-primary" onclick="location.href = 'files.php'" 
                             type="button">Page Reload</button>
                 </fieldset>
+                <hr>
+                <fieldset>
+                    <p class="alert alert-<?php echo $message['type'] ?>"><?php echo $message['text'] ?></p>
+                </fieldset>
             </form>
+            <hr>
+
+            <?php $files = glob('./uploads/*.{gif,jpg,jpeg,png}', GLOB_BRACE); ?>
+
+            <?php //for ($i = 0; $i < count($files); $i++) :  ?>
+<!-- <img src="<?php // echo $files[$i]; ?>" width="150px" height="auto"> -->
+            <?php //endfor;  ?>
+
+            <div class="row">
+                <?php foreach ($files as $file) : ?>
+                    <div class="col-12 col-sm-4 col-md-3 col-lg-3 col-xl-3">
+                        <img src="<?php echo $file; ?>" class="img-fluid img-thumbnail">
+                    </div>
+                <?php endforeach; ?>
+            </div>
+            <hr>
+            <div class="row">
+                <?php foreach ($files as $file) : ?>
+                        <img src="<?php echo $file; ?>" class="img-fluid img-thumbnail">
+                <?php endforeach; ?>
+            </div>
         </div>
-        <hr>
+
         <pre>
-<?php
-//var_dump($_FILES);
-//echo mime_content_type($dst);
-?>
+            <?php
+//            var_dump($files);
+//      echo mime_content_type($dst);
+            ?>
         </pre>
     </body>
 </html>
